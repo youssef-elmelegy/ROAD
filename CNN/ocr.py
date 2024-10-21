931bcb9075eb5c95ffb21d65e3bf3c08e5f5bbe6
JO.O PRo <115453231+youssef-elmelegy@users.noreply.github.com>
2024-10-21 09:32:09 +0300

    Add files via upload

---
+++ CNN/ocr.py
@@ -0,0 +1,51 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+import easyocr as ocr  #OCR
+import streamlit as st  #Web App
+from PIL import Image #Image Processing
+import numpy as np #Image Processing 
+
+#title
+st.title("test deploying models")
+st.markdown("## I use here `easyocr`, `streamlit`")
+st.markdown("")
+
+#image uploader
+image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
+
+
+@st.cache_resource
+def load_model(): 
+    reader = ocr.Reader(['en'],model_storage_directory='.')
+    return reader 
+
+reader = load_model() #load model
+
+if image is not None:
+
+    input_image = Image.open(image) #read image
+    st.image(input_image) #display image
+
+    with st.spinner("🤖 AI is at Work! "):
+        
+
+        result = reader.readtext(np.array(input_image))
+
+        result_text = [] #empty list for results
+
+
+        for text in result:
+            result_text.append(text[1])
+
+        st.write(result_text)
+    st.success("Here you go!")
+    # st.balloons()
+else:
+    st.write("Upload an Image")
+
+
+
+
+
+
